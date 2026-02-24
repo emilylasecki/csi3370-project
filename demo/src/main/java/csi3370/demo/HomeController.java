92671cc4eec3e77540e070d321f5caa58495c2fc
Emily Lasecki <emilylasecki@oakland.edu>
2026-02-24 14:38:18 -0500

    added sample html and css file

--- demo/src/main/java/csi3370/demo/HomeController.java
+++ demo/src/main/java/csi3370/demo/HomeController.java
@@ -1,23 +1,14 @@
 package csi3370.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return """
-               <html>
-                   <head>
-                       <title>My First Spring Boot App</title>
-                   </head>
-                   <body>
-                       <h1>Hello, Spring Boot 👋</h1>
-                       <p>No more white page error.</p>
-                   </body>
-               </html>
-               """;
+        return "index"; // maps to templates/index.html
     }
 }
