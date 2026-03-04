d7dd7038e3acd54d8726bff45e15ed137ea9a6cf
Emily Lasecki <emilylasecki@oakland.edu>
2026-03-04 18:57:56 -0500

    added renee's files to python version

--- demo/main.py
+++ demo/main.py
@@ -1,15 +1,43 @@
+# from fastapi import FastAPI, Request
+# from fastapi.templating import Jinja2Templates
+# from fastapi.staticfiles import StaticFiles
+
+# from fastapi import FastAPI
+# from app.controllers import router  # import your route definitions
+
+# app = FastAPI()
+
+# # Mount static files
+# from fastapi.staticfiles import StaticFiles
+# app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# # Include routes from controllers.py
+# app.include_router(router)
+
+# templates = Jinja2Templates(directory="templates")
+
+# @router.get("/taskcreation")
+# def about(request: Request):
+#     return templates.TemplateResponse("TaskCreation.html", {"request": request})
+
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
-from fastapi import FastAPI
-from app.controllers import router  # import your route definitions
-
 app = FastAPI()
 
 # Mount static files
-from fastapi.staticfiles import StaticFiles
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-# Include routes from controllers.py
-app.include_router(router)
+# Initialize templates (you MUST do this in every file that uses templates)
+templates = Jinja2Templates(directory="templates")
+
+# Home page route
+@app.get("/")
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+# About page route
+@app.get("/taskcreation")
+def about(request: Request):
+    return templates.TemplateResponse("TaskCreation.html", {"request": request})
